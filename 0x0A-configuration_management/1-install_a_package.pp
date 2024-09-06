5f0caf2f3eab4c3c267603fd724693fc1bff18b4
Unigwe <emmunigwe@gmail.com>
2024-09-06 15:51:36 +0100

    test

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,9 @@
 #this manifast installs flask of version 2.1.0 using pip3
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+package { 'python3-pip':
+  ensure => present,
+}
+
+exec { 'install_flask':
+  command => 'pip3 install flask==2.1.0',
+  onlyif  => 'test -z "$(pip3 show flask | grep "Version: 2.1.0")"',
 }
