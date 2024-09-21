16fefa77437240260eb619ba28bbc39d6fc314e1
Unigwe <emmunigwe@gmail.com>
2024-09-21 19:28:11 +0100

    added the puppet manifast

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,1 +1,10 @@
-ibbb
+#this puppet file make the config not accept passweordauth and make it look for the private key
+file { 2-ssh_config:
+    contant => 'Host dev
+        HostName 54.237.23.230
+        User ubuntu
+        PasswordAuthentication no
+        ChallengeResponseAuthentication no
+        IdentityFile ~/.ssh/school
+        UsePAM no'
+        }
