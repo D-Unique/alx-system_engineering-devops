250d4d26e8ec8296460d953adedb6dde3fcc3402
Unigwe <emmunigwe@gmail.com>
2024-09-21 19:31:05 +0100

    change the path to the config file in the  manifast

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,5 @@
 #this puppet file make the config not accept passweordauth and make it look for the private key
-file { 2-ssh_config:
+file { /etc/ssh/ssh_config:
     contant => 'Host dev
         HostName 54.237.23.230
         User ubuntu
