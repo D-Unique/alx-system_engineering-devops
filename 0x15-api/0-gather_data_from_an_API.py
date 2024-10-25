f89c18a71feeee1b6d17ced6b3c8811a39800eac
Unigwe <emmunigwe@gmail.com>
2024-10-25 15:27:46 +0100

    fix2(0-gather_data_from_an_API.py)

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,35 +1,38 @@
 #!/usr/bin/python3
-"""
-This module contains a Python script that returns
-information about an employee's TODO list progress,
-using this REST API, for a given employee ID
-"""
 import requests
 import sys
+
 """
-This module contains a Python script that returns
-information about an employee's TODO list progress,
-using this REST API, for a given employee ID
+This module fetches data from a specified API endpoint.
+
+Args:
+    url: The URL of the API endpoint.
+    params: Optional dictionary of query parameters.
+
+Returns:
+    A JSON object containing the fetched data.
 """
 
+def main():
+    id = sys.argv[1]
+    r = requests.get(f'https://jsonplaceholder.typicode.com/users/{id}')
+    t = requests.get(f'https://jsonplaceholder.typicode.com/users/{id}/todos')
+    data = r.json()
+    task = t.json()
+    
+    count = 0
+    total = 0
+    for item in task:
+        if item['completed'] is False:
+            total += 1
+        else:
+            total += 1
+            count += 1
+    print(f"Employee {data['name']} is done with tasks({count}/{total})")
+    for item in task:
+        print('\t', end='')
+        print(f"{item['title']}")
+    
 
-id = sys.argv[1]
-
-r = requests.get(f'https://jsonplaceholder.typicode.com/users/{id}')
-t = requests.get(f'https://jsonplaceholder.typicode.com/users/{id}/todos')
-data = r.json()
-task = t.json()
-
-count = 0
-total = 0
-for item in task:
-    if item['completed'] is False:
-        total += 1
-    else:
-        total += 1
-        count += 1
-
-print(f"Employee {data['name']} is done with tasks({count}/{total})")
-for item in task:
-    print('\t', end='')
-    print(f"{item['title']}")
+if __name__ == "__main__":
+    main()
